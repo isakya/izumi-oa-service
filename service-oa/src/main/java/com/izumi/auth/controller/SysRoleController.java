fdcfc3f7fb55602c93e4f00faeba5f973d401017
isakya <1016361268@qq.com>
2023-05-31 02:50:22 +0800

    定义统一返回结果对象

--- service-oa/src/main/java/com/izumi/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/izumi/auth/controller/SysRoleController.java
@@ -1,6 +1,7 @@
 package com.izumi.auth.controller;
 
 import com.izumi.auth.service.SysRoleService;
+import com.izumi.common.result.Result;
 import com.izumi.model.system.SysRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,9 @@ public class SysRoleController {
     // http:localhost:8800/admin/system/sysRole/findAll
     // 查询所有的角色
     @GetMapping("/findAll")
-    public List<SysRole> findAll() {
+    public Result findAll() {
         // 调用service的方法
         List<SysRole> list = sysRoleService.list();
-        return list;
+        return  Result.ok(list);
     }
 }
