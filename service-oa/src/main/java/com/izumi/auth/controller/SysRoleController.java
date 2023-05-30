630b65edd039e3ae92bec2464a4311e3193f53df
isakya <1016361268@qq.com>
2023-05-31 02:20:03 +0800

    测试角色接口返回所有数据

---
+++ service-oa/src/main/java/com/izumi/auth/controller/SysRoleController.java
@@ -0,0 +1,26 @@
+package com.izumi.auth.controller;
+
+import com.izumi.auth.service.SysRoleService;
+import com.izumi.model.system.SysRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/system/sysRole")
+public class SysRoleController {
+    // 注入service
+    @Autowired
+    private SysRoleService sysRoleService;
+    // http:localhost:8800/admin/system/sysRole/findAll
+    // 查询所有的角色
+    @GetMapping("/findAll")
+    public List<SysRole> findAll() {
+        // 调用service的方法
+        List<SysRole> list = sysRoleService.list();
+        return list;
+    }
+}
