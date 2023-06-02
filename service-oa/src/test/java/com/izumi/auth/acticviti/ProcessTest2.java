acc890b50403b274dd1292546b3a93523ea452eb
isakya <1016361268@qq.com>
2023-06-03 02:30:59 +0800

    UEL-value、UEL-method方式分配任务

---
+++ service-oa/src/test/java/com/izumi/auth/acticviti/ProcessTest2.java
@@ -0,0 +1,107 @@
+package com.izumi.auth.acticviti;
+
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+public class ProcessTest2 {
+    // 注入RepositoryService
+    @Autowired
+    private RepositoryService repositoryService;
+
+    // 注入
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private HistoryService historyService;
+
+    ///////////////////////////////////////////////
+    // UEL-value 方式
+    @Test
+    public void deployProcess01() {
+        // 流程部署
+        Deployment deploy = repositoryService.createDeployment()
+                .addClasspathResource("process/jiaban.bpmn20.xml")
+                .addClasspathResource("process/jiaban.png")
+                .name("加班申请流程")
+                .deploy();
+        System.out.println(deploy.getId());
+        System.out.println(deploy.getName());
+    }
+
+    /**
+     * 启动流程实例
+     */
+    @Test
+    public void startUpProcess01() {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("assignee1","zhangsan");
+        variables.put("assignee2","lisi");
+        //创建流程实例,我们需要知道流程定义的key
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("jiaban", variables);
+        //输出实例的相关信息
+        System.out.println("流程定义id：" + processInstance.getProcessDefinitionId());
+        System.out.println("流程实例id：" + processInstance.getId());
+    }
+
+    // 查询个人的代办任务--zhangsan
+    @Test
+    public void findTaskList() {
+        String assignee = "zhangsan";
+        List<Task> list = taskService.createTaskQuery()
+                .taskAssignee(assignee)
+                .list();
+        for(Task task : list) {
+            System.out.println("流程实例id：" + task.getProcessInstanceId());
+            System.out.println("任务id：" + task.getId());
+            System.out.println("任务负责人：" + task.getAssignee());
+            System.out.println("任务名称：" + task.getName());
+        }
+    }
+
+
+
+
+    ///////////////////////////////////////////////
+    // UEL-method 方式
+    @Test
+    public void deployProcess02() {
+        // 流程部署
+        Deployment deploy = repositoryService.createDeployment()
+                .addClasspathResource("process/jiaban02.bpmn20.xml")
+                .name("加班申请流程02")
+                .deploy();
+        System.out.println(deploy.getId());
+        System.out.println(deploy.getName());
+    }
+
+    /**
+     * 启动流程实例
+     */
+    @Test
+    public void startUpProcess02() {
+        //创建流程实例,我们需要知道流程定义的key
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("jiaban");
+        //输出实例的相关信息
+        System.out.println("流程定义02id：" + processInstance.getProcessDefinitionId());
+        System.out.println("流程实例02id：" + processInstance.getId());
+    }
+}
---
+++ service-oa/src/test/java/com/izumi/auth/acticviti/UserBean.java
@@ -0,0 +1,17 @@
+package com.izumi.auth.acticviti;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserBean {
+
+    public String getUsername(int id) {
+        if(id == 1) {
+            return "zhangsan";
+        }
+        if(id == 2) {
+            return "lisi";
+        }
+        return "admin";
+    }
+}
