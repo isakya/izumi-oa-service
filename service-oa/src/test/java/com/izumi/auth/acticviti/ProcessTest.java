8de147cc23640f81be663fd4518ba407b2b4ec49
isakya <1016361268@qq.com>
2023-06-03 02:01:15 +0800

    让实际业务与activiti表关联(BusinessKey)、挂起，激活流程实例

--- service-oa/src/test/java/com/izumi/auth/acticviti/ProcessTest.java
+++ service-oa/src/test/java/com/izumi/auth/acticviti/ProcessTest.java
@@ -6,6 +6,7 @@ import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.history.HistoricTaskInstance;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,58 @@ public class ProcessTest {
 
     @Autowired
     private HistoryService historyService;
+
+
+    /**
+     * 单个流程实例挂起
+     */
+    @Test
+    public void SingleSuspendProcessInstance() {
+        String processInstanceId = "8bdff984-ab53-11ed-9b17-f8e43b734677";
+        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+        //获取到当前流程定义是否为暂停状态   suspended方法为true代表为暂停   false就是运行的
+        boolean suspended = processInstance.isSuspended();
+        if (suspended) {
+            runtimeService.activateProcessInstanceById(processInstanceId);
+            System.out.println("流程实例:" + processInstanceId + "激活");
+        } else {
+            runtimeService.suspendProcessInstanceById(processInstanceId);
+            System.out.println("流程实例:" + processInstanceId + "挂起");
+        }
+    }
+
+    /**
+     * 全部流程实例挂起
+     */
+    @Test
+    public void suspendProcessInstance() {
+        // 1. 获取流程定义的对象
+        ProcessDefinition qingjia = repositoryService.createProcessDefinitionQuery().processDefinitionKey("qingjia").singleResult();
+        // 2 获取到当前流程定义是否为暂停状态 suspended方法为true是暂停的，suspended方法为false是运行的
+        boolean suspended = qingjia.isSuspended();
+        if (suspended) {
+            // 暂定,那就可以激活
+            // 参数1:流程定义的id  参数2:是否激活    参数3:时间点
+            repositoryService.activateProcessDefinitionById(qingjia.getId(), true, null);
+            System.out.println("流程定义:" + qingjia.getId() + "激活");
+        } else {
+            repositoryService.suspendProcessDefinitionById(qingjia.getId(), true, null);
+            System.out.println("流程定义:" + qingjia.getId() + "挂起");
+        }
+    }
+
+    /**
+     * 启动流程实例，添加businessKey
+     */
+    @Test
+    public void startUpProcessAddBusinessKey(){
+        String businessKey = "1";
+        // 启动流程实例，指定业务标识businessKey，也就是请假申请单id
+        ProcessInstance processInstance = runtimeService.
+                startProcessInstanceByKey("qingjia",businessKey);
+        // 输出
+        System.out.println("业务id:"+processInstance.getBusinessKey());
+    }
 
     /**
      * 5 查询已处理历史任务
